ec6b8e3c16e657fd65d297c30cc165dc9ef439b1
knife-kis <36123852+knife-kis@users.noreply.github.com>
2025-10-11 14:25:28 +0700

    рефакторинг внешнего вида

--- src/main/java/ru/citlab24/protokol/Main.java
+++ src/main/java/ru/citlab24/protokol/Main.java
@@ -8,18 +8,30 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("ru", "RU"));
         SwingUtilities.invokeLater(() -> {
-            // Устанавливаем современную тему
-            FlatLightLaf.setup();
+            SwingUtilities.invokeLater(() -> {
+                // Гладкие шрифты + современные оконные декорации
+                System.setProperty("awt.useSystemAAFontSettings", "on");
+                System.setProperty("swing.aatext", "true");
+                System.setProperty("flatlaf.useWindowDecorations", "true");
+                com.formdev.flatlaf.FlatLaf.setUseNativeWindowDecorations(true);
 
-            // Настройка глобальных параметров
-            UIManager.put("Button.arc", 8);
-            UIManager.put("Component.arc", 8);
-            UIManager.put("TextComponent.arc", 5);
-            UIManager.put("ProgressBar.arc", 8);
-            UIManager.put("ScrollBar.width", 14);
+                // Светлая по умолчанию (можно FlatDarkLaf / FlatIntelliJLaf / FlatDarculaLaf)
+                UIManager.put("Component.arc", 12);
+                UIManager.put("Button.arc", 12);
+                UIManager.put("TextComponent.arc", 12);
+                UIManager.put("ScrollBar.width", 14);
+                UIManager.put("ScrollBar.thumbArc", 999); // «пилюля»
+                UIManager.put("Component.focusWidth", 1);
 
-            MainFrame frame = new MainFrame();
-            frame.setVisible(true);
+                try {
+                    UIManager.setLookAndFeel(new com.formdev.flatlaf.FlatIntelliJLaf());
+                    com.formdev.flatlaf.FlatLaf.updateUI();
+                } catch (Exception ignore) {}
+
+                MainFrame frame = new MainFrame();
+                frame.setLocationByPlatform(true);
+                frame.setVisible(true);
+            });
         });
     }
 }
