d10b98756ffea774691331d922b4aa247c5e78fb
knife-kis <tarnovskiym@gmail.com>
2026-01-21 13:09:14 +0700

    какая то туфта

--- src/main/java/ru/citlab24/protokol/export/PrintSetupUtils.java
+++ src/main/java/ru/citlab24/protokol/export/PrintSetupUtils.java
@@ -2,8 +2,10 @@ package ru.citlab24.protokol.export;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.xmlbeans.XmlCursor;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTPageSetup;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.STDuplex;
+
+import javax.xml.namespace.QName;
 
 public final class PrintSetupUtils {
     private PrintSetupUtils() {}
@@ -13,9 +15,24 @@ public final class PrintSetupUtils {
             return;
         }
         XSSFSheet xssfSheet = (XSSFSheet) sheet;
+
         CTPageSetup pageSetup = xssfSheet.getCTWorksheet().isSetPageSetup()
                 ? xssfSheet.getCTWorksheet().getPageSetup()
                 : xssfSheet.getCTWorksheet().addNewPageSetup();
-        pageSetup.setDuplex(STDuplex.SHORT);
+
+        // Ставим атрибут duplex напрямую, без STDuplex (которого может не быть в схемах)
+        XmlCursor cursor = null;
+        try {
+            cursor = pageSetup.newCursor();
+            cursor.toStartDoc();
+            cursor.toFirstChild(); // <pageSetup .../>
+            cursor.setAttributeText(new QName("", "duplex"), "duplexShortEdge");
+        } catch (Throwable ignore) {
+            // не валим экспорт
+        } finally {
+            if (cursor != null) {
+                cursor.dispose();
+            }
+        }
     }
 }
