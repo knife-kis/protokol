3840d7614df614cfdcc1436b54557ef47ef33611
knife-kis <tarnovskiym@gmail.com>
2026-02-05 13:06:47 +0700

    1.2.2

--- src/main/java/ru/citlab24/protokol/MainFrame.java
+++ src/main/java/ru/citlab24/protokol/MainFrame.java
@@ -57,7 +57,7 @@ public class MainFrame extends JFrame {
         menuBar.add(Box.createHorizontalStrut(8));
         menuBar.add(createProjectMenu(onLoadProject, onSaveProject));
         menuBar.add(Box.createHorizontalStrut(12));
-        JLabel versionLabel = new JLabel("v 1.2.1");
+        JLabel versionLabel = new JLabel("v 1.2.2");
         versionLabel.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 0));
         menuBar.add(versionLabel);
         return menuBar;
