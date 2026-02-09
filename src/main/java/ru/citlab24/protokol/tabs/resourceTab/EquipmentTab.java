d39f6d7c40f83a4388fc2ae63f2e0155413daa60
knife-kis <36123852+knife-kis@users.noreply.github.com>
2026-02-09 15:47:34 +0700

    Разделил меню ресурса на персонал и оборудование

---
+++ src/main/java/ru/citlab24/protokol/tabs/resourceTab/EquipmentTab.java
@@ -0,0 +1,12 @@
+package ru.citlab24.protokol.tabs.resourceTab;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EquipmentTab extends JPanel {
+
+    public EquipmentTab() {
+        super(new GridBagLayout());
+        add(new JLabel("Раздел оборудования будет добавлен позже"));
+    }
+}
